ab37197792d2d1259be167f1ef94521474e701a9
Reagan Ronald <reaganronashi@gmail.com>
2023-07-02 15:14:50 +0300

    mods

--- Roshifood/food/views.py
+++ Roshifood/food/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+    identify = {'name':'Ronashi', 'email':'Ronashi@gmail.com', 'msg':'have my way in this world'}
+    return render(request, 'food/index.html', identify)
+
+def pizza(request):
+    return render(request, 'food/pizza.html')
