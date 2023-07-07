6c527d4e637406850ba10e4fffc5e52e43c2fb35
Reagan Ronald <reaganronashi@gmail.com>
2023-07-07 23:28:09 +0300

    gesmods

--- Roshifood/food/models.py
+++ Roshifood/food/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 
-class pizza(models.Model):
+class Pizza(models.Model):
     name = models.CharField(max_length=100)
     priceM = models.DecimalField(max_digits=4, decimal_places=2)
     priceL = models.DecimalField(max_digits=4, decimal_places=2)
     pImage = models.URLField()
     
  
-class burger(models.Model):
+class Burger(models.Model):
     name = models.CharField(max_length=100)
     priceM = models.DecimalField(max_digits=4, decimal_places=2)
     priceL = models.DecimalField(max_digits=4, decimal_places=2)
     pImage = models.URLField()
+    
--- Roshifood/food/views.py
+++ Roshifood/food/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import pizza, burger
+from .models import Pizza, Burger
 
 
 def index(request):
     return render(request, 'food/index.html')
 
 def pizza(request):
-    pizzas = pizzas.objects.all()
+    pizzas = Pizza.objects.all()
     context =  {'pizzas': pizzas}
+    print(pizzas)
     return render(request, 'food/pizza.html', context)
+
 
 # def burger(request):
 #     return render(request, 'food/burger.html')
